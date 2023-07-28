6fbc7a432c7eb5fb4c85324df54937a6200263b7
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-07-28 21:57:46 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h2 className='text-primary border-bottom border-3 border-warning pb-2 position-absolute top-50 start-50 translate-middle'>Welcome to Home !</h2>
+    </div>
+  )
+}
+
+export default Home
